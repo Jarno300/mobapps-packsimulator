1a749e145ae92c5a4370ec66f5e2510a5110649f
dkmattiUCLL <r0801627@gmail.com>
2025-12-29 22:52:12 +0100

    updated boosterpacks save to local storage

--- pack-open-sim/api/fetchBoosterPacks.ts
+++ pack-open-sim/api/fetchBoosterPacks.ts
@@ -1,19 +1,19 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { BoosterPack } from '@/components/pokémon-related-components/booster-pack';
+import { BoosterPack, createBoosterPack } from '@/components/pokémon-related-components/booster-pack';
 
 const STORAGE_KEY = "@my_app_booster_packs";
 
-// 1. Sla packs op (bijv. na het kopen/krijgen)
+
 export const savePacks = async (allPacks: BoosterPack[]) => {
     try {
         const jsonValue = JSON.stringify(allPacks);
         await AsyncStorage.setItem(STORAGE_KEY, jsonValue);
     } catch (e) {
-        console.error("Fout bij opslaan op Android", e);
+        console.error("Fout bij opslaan Boosterpacks", e);
     }
 };
 
-// 2. Haal het aantal packs op bij opstarten
+
 export const getPacksFromStorage = async (): Promise<BoosterPack[]> => {
     try {
         const jsonValue = await AsyncStorage.getItem(STORAGE_KEY);
@@ -21,4 +21,17 @@ export const getPacksFromStorage = async (): Promise<BoosterPack[]> => {
     } catch (e) {
         return [];
     }
+};
+
+
+export const addAndSaveNewPack = async (id: number, name: string, image: string) => {
+    const newPack = createBoosterPack(id, name, image);
+
+    const currentPacks = await getPacksFromStorage();
+
+    const updatedPacks = [...currentPacks, newPack];
+
+
+    await savePacks(updatedPacks);
+
 };
--- pack-open-sim/components/pokémon-related-components/booster-pack.ts
+++ pack-open-sim/components/pokémon-related-components/booster-pack.ts
@@ -1,10 +1,32 @@
 import { Card } from "@/api/fetchCards";
+import { getCardCache } from "@/cache/setCardCache";
 
 export interface BoosterPack {
-    id: string;
+    id: number;
     name: string;
     image?: string;
     cards: Card[];
     isOpened: boolean;
+
+}
+
+export const createBoosterPack = (id: number, name: string, image: string): BoosterPack => {
+    const cardList = getCardCache();
+    const cardRandomizer: Card[] = [];
+    for (let i = 0; i < 10; i++) {
+        const randomIndex = Math.floor(Math.random() * cardList.length);
+        cardRandomizer.push(cardList[randomIndex]);
+    }
+    const newPack: BoosterPack = {
+        id,
+        name,
+        image,
+        cards: cardRandomizer,
+        isOpened: false
+    };
+
+    return newPack;
+
+
 
 }
